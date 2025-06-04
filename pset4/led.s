6039310861ed5f25bb44fe0946078d83979a23a3
mattkup1 <104730300+mattkup1@users.noreply.github.com>
2025-06-04 08:44:50 +0300

    Update led.s, Shortened and improved code
    Moved call to print loop to top of main
    used sll instead of loop
    changed all instances of j to jal

--- pset4/led.s
+++ pset4/led.s
@@ -1,8 +1,11 @@
 .text
 
 li s0,0                                 # Initialize LED register with 0
+li a0,0
 
 main:
+    jal ra,print_led                    # Print LED's
+
     li a7,12                            # Read command from user
     ecall
 
@@ -15,7 +18,7 @@ main:
     beq a1,t0,read_led_pos
     li t0,'t'
     beq a1,t0,read_led_pos
-    j bottom_main
+    jal bottom_main
 
     
     read_led_pos:                       # Read bit position from user
@@ -27,16 +30,16 @@ bottom_main:
     mv a0,s0                            # Copy LED register to a0
     jal led_control                     # Execute command via led_control function with arguments a0, a1, a2
     mv s0,a0                            # Copy returned LED register a0 back to s0
-    jal print_led
-    j main
+    jal main
 
 led_control:                            # led_control function body
     addi sp,sp,-24                      # Memory management
-    sw a0,20(sp)
-    sw a1,16(sp)
-    sw a2,12(sp)
-    sw t0,8(sp)
-    sw t1,4(sp)
+    sw ra,20(sp)
+    sw a0,16(sp)
+    sw a1,12(sp)
+    sw a2,8(sp)
+    sw t0,4(sp)
+    sw t1,0(sp)
 
     li t0,'s'                           # Case set
     beq a1,t0,set_or_toggle
@@ -60,74 +63,66 @@ led_control:                            # led_control function body
     beq a1,t0,rotate_left
 
     end_led_ctrl:
-    lw t1,4(sp)
-    lw t0,8(sp)                         # Restore register values
-    lw a2,12(sp)
-    lw a1,16(sp)
+    lw t1,0(sp)
+    lw t0,4(sp)                         # Restore register values
+    lw a2,8(sp)
+    lw a1,12(sp)
+    lw ra,20(sp)
     addi sp,sp,24
     ret                                 # Return
 
 
-set_or_toggle:
+set_or_toggle:                          # Handle set and toggle cases
     li t0,1                             # Set t0 LSB to 1 and all other bits to 0
-    set_loop:                           # Shift t0 left and set the 1 bit to argumented position
-        beq a2,zero,end_set_loop        # Break once the 1 bit in t0 is in the correct position
-        addi a2,a2,-1                   # Decrement iterator
-        slli t0,t0,1                    # Shift t0 right by 1
-        j set_loop
+    sll t0,t0,a2
 
-    end_set_loop:
-        li t1,'s'                       # Case set
-        beq a1,t1,skip_toggle
+    li t1,'s'                           # Case set
+    beq a1,t1,skip_toggle
     toggle:                             # Toggle LED at argumented position
         xor a0,t0,a0                    
-        j end_led_ctrl
+        jal end_led_ctrl
     skip_toggle:                        # Turn on LED at argumented position
         or a0,t0,a0
-        j end_led_ctrl
+        jal end_led_ctrl
 
 clear:
-    li t0,0xfffffffe                    # Set t0 MSB to 0 and all other bits to 1
-    clear_loop:                         # Shift t0 right and set the 0 bit to argumented position
-        beq a2,zero,end_clear_loop      
-        slli t0,t0,1                    # Shift t0 left by 1
-        addi t0,t0,1                    # Set new LSB to 1
-        addi a2,a2,-1                   # Decrement iterator (argumented)
-        j clear_loop
-    end_clear_loop:
-        and a0,t0,a0                    # Perform bitwise and between shifted number and led register to clear argumented led
-        j end_led_ctrl
+    li t0,1                             # Set t0 LSB to 1 and all other bits to 0
+    sll t0,t0,a2                        # Shift the 1 bit to argumented bit position, Fill spaces with 0
+    not t0,t0                           # Invert t0, Bit at argumented position will be 0 and all other bits will be 1
+    and a0,t0,a0                        # Perform bitwise and between t0 and led register to clear argumented led
+    jal end_led_ctrl
 
 shift_left:
     slli a0,a0,1                        # Shift LED register left by 1
-    j end_led_ctrl
+    jal end_led_ctrl
 
 shift_right:
     srli a0,a0,1                        # Shift LED register right by 1
-    j end_led_ctrl
+    jal end_led_ctrl
 
 rotate_left:                            
     srli t0,a0,31                       # Store LED register MSB in t0
     slli a0,a0,1                        # Shift LED register left by 1, LSB will be 0
     add a0,a0,t0                        # Add stored MSB to shifted LED register, Effectively moving the MSB to the LSB position
-    j end_led_ctrl
+    jal end_led_ctrl
 
 rotate_right:
     andi t0,a0,1                        # Store LED register LSB in t0
     slli t0,t0,31                       # Move stored LSB from t0 LSB t0 MSB
     srli a0,a0,1                        # Shift LED register right by 1, MSB will be 0
     add a0,a0,t0                        # Add stored LSB (stored in t0 MSB) to shifted LED register - Effectively moving the LSB to the MSB position
-    j end_led_ctrl  
+    jal end_led_ctrl  
 
 
 print_led:
-    addi sp,sp,-24                      # Memory management
-    sw a0,20(sp)
-    sw a1,16(sp)
-    sw a2,12(sp)
-    sw t0,8(sp)
-    sw t1,4(sp)
-    sw t2,0(sp)
+    addi sp,sp,-32                      # Memory management
+    sw ra,28(sp)
+    sw a0,24(sp)
+    sw a1,20(sp)
+    sw a2,16(sp)
+    sw t0,12(sp)
+    sw t1,8(sp)
+    sw t2,4(sp)
 
     mv t0,a0                            # Copy LED register from a0 to t0, as a0 will be used for syscalls
     li t1,32                            # Set iterator to amount of iterations (32)
@@ -141,27 +136,26 @@ print_led:
         li a7,11                        # Else, LED is on - Print #
         li a0,'#'
         ecall
-        j print_loop
+        jal print_loop
 
         print_off:
             li a7,11                    # Print '.' for off
             li a0,'.'
             ecall
 
-            j print_loop
+            jal print_loop
 
     end_print:
         li a7,11                        # Print newline
         li a0,10                        # Load newline ASCII code to a0
         ecall
 
-        lw t2,0(sp)                     # Restore register values
-        lw t1,4(sp)
-        lw t0,8(sp)
-        lw a2,12(sp)
-        lw a1,16(sp)
-        lw a0,20(sp)
-        addi sp,sp,24
+        lw t2,4(sp)                     # Restore register values
+        lw t1,8(sp)
+        lw t0,12(sp)
+        lw a2,16(sp)
+        lw a1,20(sp)
+        lw a0,24(sp)
+        lw ra,28(sp)
+        addi sp,sp,32
         ret                             # Return
-
-    
